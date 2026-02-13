e42b080b80545536dff3198f0e51dbd9ff20099f
vmadhanprabu96-ops <vmadhanprabu96@gmail.com>
2026-02-14 05:06:59 +0530

    Add files via upload

--- student.js
+++ student.js
@@ -1,3 +1,29 @@
+// List of notes in GitHub notes folder
+const notes = [
+  {
+    name: "Environmental Chemistry Chapter I & II",
+    file: "notes/Environ Chem Chap I & II , word 2003.doc"
+  },
+  {
+    name: "Functional and Non Functional Requirements",
+    file: "notes/Functional and non functional requirements.ppt"
+  }
+];
 
-const notesDiv = document.getElementById("notes");
-notesDiv.innerHTML = "<p>Notes will appear here after upload.</p>";
+// Display notes
+const container = document.getElementById("notes");
+
+notes.forEach(note => {
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+    <p>
+      📄 ${note.name}
+      <br>
+      <a href="${note.file}" target="_blank">View</a> |
+      <a href="${note.file}" download>Download</a>
+    </p>
+  `;
+
+  container.appendChild(div);
+});
